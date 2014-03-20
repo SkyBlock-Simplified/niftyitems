2d7c1770c285739f504ceb7f56ee0b3af676dad0
Brian Graham <support@netcoding.net>
2014-03-19 21:27:24 -0400

    Added new command definitions

--- src/net/netcoding/niftyitems/NiftyItems.java
+++ src/net/netcoding/niftyitems/NiftyItems.java
@@ -1,10 +1,10 @@
 package net.netcoding.niftyitems;
 
 import net.netcoding.niftybukkit.minecraft.BukkitPlugin;
-import net.netcoding.niftyitems.commands.ClearInv;
+import net.netcoding.niftyitems.commands.ClearInventory;
 import net.netcoding.niftyitems.commands.ClearLore;
 import net.netcoding.niftyitems.commands.GameMode;
-import net.netcoding.niftyitems.commands.IReload;
+import net.netcoding.niftyitems.commands.ItemReload;
 import net.netcoding.niftyitems.commands.Item;
 import net.netcoding.niftyitems.commands.ItemDb;
 import net.netcoding.niftyitems.listeners.Inventory;
@@ -21,12 +21,12 @@ public class NiftyItems extends BukkitPlugin {
 		Cache.Settings.reload();
 
 		this.getLog().console("Registering Commands");
-		new ClearInv(this);
+		new ClearInventory(this);
 		new ClearLore(this);
 		new GameMode(this);
 		new Item(this);
 		new ItemDb(this);
-		new IReload(this);
+		new ItemReload(this);
 
 		this.getLog().console("Registering Listeners");
 		new Inventory(this);
