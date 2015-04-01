3581385f92b2eac8477ee5eed2821003fe3bb5f0
Brian Graham <support@netcoding.net>
2015-03-31 20:50:21 -0400

    Now supports clearing the lore of other players

--- src/net/netcoding/niftyitems/commands/ClearLore.java
+++ src/net/netcoding/niftyitems/commands/ClearLore.java
@@ -1,57 +1,43 @@
 package net.netcoding.niftyitems.commands;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.netcoding.niftybukkit.minecraft.BukkitCommand;
 import net.netcoding.niftyitems.managers.Lore;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ClearLore extends BukkitCommand {
 
 	public ClearLore(JavaPlugin plugin) {
 		super(plugin, "clearlore");
-		this.isPlayerOnly();
+		this.setPlayerOnly();
 		this.setMinimumArgsLength(0);
-		this.setMaximumArgsLength(1);
-	}
-
-	private boolean revertItem(ItemStack item) {
-		if (!Lore.isRestricted(item).equalsIgnoreCase("none") && item.getItemMeta().hasLore()) {
-			ItemMeta itemMeta = item.getItemMeta();
-			List<String> lores = itemMeta.getLore();
-			List<String> notOurs = new ArrayList<>();
-
-			for (String lore : lores) {
-				if (!lore.startsWith(Lore.getLore("creative")) && !lore.startsWith(Lore.getLore("spawned")))
-					notOurs.add(lore);
-			}
-
-			itemMeta.setLore(notOurs);
-			item.setItemMeta(itemMeta);
-			return true;
-		}
-
-		return false;
 	}
 
 	@Override
 	public void onCommand(CommandSender sender, String alias, String[] args) throws Exception {
-		Player player = (Player)sender;
+		if (isConsole(sender) && args.length < 2) {
+			this.getLog().error(sender, "You must pass a player name when clearing an items lore");
+			return;
+		}
+
+		Player player = args.length == 1 ? (Player)sender : findPlayer(args[0]);
+
+		if (player == null) {
+			this.getLog().error(sender, "Unable to locate player matching {{0}}!", args[0]);
+			return;
+		}
 
 		if (args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("hand"))) {
-			if (this.revertItem(player.getItemInHand()))
+			if (Lore.revert(player.getItemInHand()))
 				this.getLog().message(sender, "The item {{0}} has had its lore removed.", player.getItemInHand().getType().name());
 			else
 				this.getLog().error(sender, "The item {{0}} has no lore.", player.getItemInHand().getType().name());
 		} else if (args.length == 1 && args[0].equalsIgnoreCase("all")) {
 			for (ItemStack item : player.getInventory().getContents())
-				this.revertItem(item);
+				Lore.revert(item);
 
 			this.getLog().message(sender, "All items in your inventory have had their removed.");
 		} else
