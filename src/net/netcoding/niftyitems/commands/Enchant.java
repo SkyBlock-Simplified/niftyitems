a03c715c934bd5440110cf004b1bace2c52c7033
Brian Graham <support@netcoding.net>
2015-04-01 18:49:56 -0400

    Now shows possible enchantments

--- src/net/netcoding/niftyitems/commands/Enchant.java
+++ src/net/netcoding/niftyitems/commands/Enchant.java
@@ -7,8 +7,10 @@ import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.inventory.enchantments.EnchantmentData;
 import net.netcoding.niftybukkit.inventory.items.ItemData;
 import net.netcoding.niftybukkit.minecraft.BukkitCommand;
+import net.netcoding.niftybukkit.util.ListUtil;
 import net.netcoding.niftybukkit.util.StringUtil;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -19,6 +21,7 @@ public class Enchant extends BukkitCommand {
 	public Enchant(JavaPlugin plugin) {
 		super(plugin, "enchant");
 		this.setPlayerOnly();
+		this.setMinimumArgsLength(0);
 	}
 
 	@Override
@@ -31,23 +34,47 @@ public class Enchant extends BukkitCommand {
 			return;
 		}
 
-		if (args[0].matches("^fake|hidden|glow$")) {
-			stack = ItemData.addGlow(stack);
-			this.getLog().message(sender, "Your item now glows!");
-		} else {
-			for (EnchantmentData data : NiftyBukkit.getEnchantmentDatabase().parse(args)) {
-				try {
-					if (data.apply(stack, alias.matches("^ue(nchant)?$")))
-						enchants.add(StringUtil.format("{{0}} at level {{1}}.", data.getName(), data.getUserLevel()));
-				} catch (Exception ex) { }
-			}
+		if (ListUtil.isEmpty(args)) {
+			List<EnchantmentData> possibleEnchants = NiftyBukkit.getEnchantmentDatabase().getPossibleEnchants(stack);
 
-			if (enchants.size() == 0)
-				this.getLog().error(sender, "Unable to apply any enchants, please check your previous command!");
+			if (ListUtil.isEmpty(possibleEnchants))
+				this.getLog().error(sender, "The {0} {{1}} in your hand cannot normally be enchanted!", (stack.getType().isBlock() ? "block" : "item"), stack.getType().name());
 			else {
-				this.getLog().message(sender, "Your {{0}} has been given the following enchantments:", NiftyBukkit.getItemDatabase().name(stack));
-				for (String enchant : enchants)
-					this.getLog().message(sender, enchant);
+				List<String> names = new ArrayList<>();
+
+				for (EnchantmentData enchData : possibleEnchants) {
+					List<String> enchNames = enchData.getNames();
+
+					for (int i = 0; i < (enchNames.size() < 2 ? enchNames.size() : 2); i++)
+						names.add(enchNames.get(i));
+				}
+
+				if (ListUtil.isEmpty(names))
+					this.getLog().error(sender, "Unable to locate any possible enchantments for {{0}}!", stack.getType().name());
+				else {
+					this.getLog().message(sender, "[{{0}}]", "Possible Enchantments");
+					this.getLog().message(sender, "{{0}}", StringUtil.implode(ChatColor.GRAY + ", " + ChatColor.RED, names));
+				}
+			}
+		} else {
+			if (args[0].matches("^fake|hidden|glow$")) {
+				stack = ItemData.addGlow(stack);
+				this.getLog().message(sender, "Your item now glows!");
+			} else {
+				for (EnchantmentData data : NiftyBukkit.getEnchantmentDatabase().parse(args)) {
+					try {
+						if (data.apply(stack, alias.matches("^ue(nchant)?$")))
+							enchants.add(StringUtil.format("{{0}} at level {{1}}.", data.getName(), data.getUserLevel()));
+					} catch (Exception ex) { }
+				}
+
+				if (enchants.size() == 0)
+					this.getLog().error(sender, "Unable to apply any enchants, please check your previous command!");
+				else {
+					this.getLog().message(sender, "Your {{0}} has been given the following enchantments:", NiftyBukkit.getItemDatabase().name(stack));
+					for (String enchant : enchants)
+						this.getLog().message(sender, enchant);
+				}
 			}
 		}
 	}
