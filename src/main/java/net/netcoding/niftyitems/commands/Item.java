a94dc55db1ca2e6731f3ec7cf1de9e10c9d1fdd3
Brian Graham <support@netcoding.net>
2016-01-18 20:17:32 -0500

    Fixed give/item command

--- src/main/java/net/netcoding/niftyitems/commands/Item.java
+++ src/main/java/net/netcoding/niftyitems/commands/Item.java
@@ -3,58 +3,68 @@ package net.netcoding.niftyitems.commands;
 import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.inventory.InventoryWorkaround;
 import net.netcoding.niftybukkit.minecraft.BukkitCommand;
+import net.netcoding.niftybukkit.mojang.BukkitMojangProfile;
+import net.netcoding.niftycore.mojang.exceptions.ProfileNotFoundException;
 import net.netcoding.niftycore.util.NumberUtil;
 import net.netcoding.niftycore.util.StringUtil;
 import net.netcoding.niftyitems.NiftyItems;
 import net.netcoding.niftyitems.cache.Config;
 import net.netcoding.niftyitems.managers.Lore;
-
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Give extends BukkitCommand {
+public class Item extends BukkitCommand {
 
-	public Give(JavaPlugin plugin) {
-		super(plugin, "give");
+	public Item(JavaPlugin plugin) {
+		super(plugin, "item");
 		this.setCheckPerms(false);
 		this.setCheckHelp(false);
-		this.editUsage(0, "item", "<type>[:datavalue] [amount]");
-		this.editUsage(0, "i", "<type>[:datavalue] [amount]");
+		this.editUsage(0, "give", "<player> <type>[:datavalue] [amount]");
+		this.editUsage(0, "g", "<player> <type>[:datavalue] [amount]");
 	}
 
 	@Override
 	public void onCommand(CommandSender sender, String alias, String[] args) throws Exception {
 		boolean isGive = alias.matches("^g(ive)?$");
-		boolean isItem = alias.matches("^i(tem)?$");
+		boolean isItem = !isGive;
+		BukkitMojangProfile receiver;
 
-		if (isGive && findPlayer(args[0]) == null) {
-			this.getLog().error(sender, "You must pass the player name when giving items!");
+		if (!isGive && isConsole(sender)) {
+			this.getLog().error(sender, "You cannot use the item command from console!");
 			return;
-		} else if (isItem && findPlayer(args[0]) != null)
-			args = StringUtil.implode(",", args, 1, args.length - 1).split(",");
-
-		String user = (isGive ? args[0] : sender.getName());
-		Player player = findPlayer(user);
-		if (isGive) args = StringUtil.implode(",", args, 1, args.length - 1).split(",");
-		ItemStack stack;
+		}
 
 		if (isGive && !this.hasPermissions(sender, "item", "give")) {
 			this.getLog().error(sender, "You are not allowed to give items!");
 			return;
 		}
 
-		if (isConsole(user)) {
-			this.getLog().error(sender, "You cannot give the console items!");
+		try {
+			receiver = NiftyBukkit.getMojangRepository().searchByUsername(isGive ? args[0] : sender.getName());
+		} catch (ProfileNotFoundException pnfex) {
+			this.getLog().error(sender, "Unable to locate profile for {{0}}!", args[0]);
 			return;
 		}
+
+		if (isGive || !receiver.getName().equals(sender.getName())) {
+			args = StringUtil.split(",", StringUtil.implode(",", args, 1));
+
+			if (args.length == 0) {
+				this.showUsage(sender);
+				return;
+			}
+		}
+
+		ItemStack stack;
+		Player player = receiver.getOfflinePlayer().getPlayer();
 
 		try {
 			stack = NiftyBukkit.getItemDatabase().get(args[0]);
 
-			if (Material.AIR.equals(stack.getType())) {
+			if (Material.AIR == stack.getType()) {
 				this.getLog().error(sender, "You cannot spawn {0}!", "AIR");
 				return;
 			}
@@ -64,8 +74,8 @@ public class Give extends BukkitCommand {
 		}
 
 		String displayName = NiftyBukkit.getItemDatabase().name(stack);
-		args = StringUtil.split(",", StringUtil.implode(",", args, 1, args.length));
-		int amount = 1;
+		args = StringUtil.split(",", StringUtil.implode(",", args, 1));
+		int amount = -1;
 
 		if (isItem || (NiftyItems.getPluginConfig().giveEnforcesBlacklist() && !this.hasPermissions(sender, "bypass", "give"))) {
 			if (NiftyItems.getPluginConfig().isBlacklisted(player, stack, "spawned") || (!this.hasPermissions(player, "item") && !NiftyItems.getPluginConfig().hasBypass(player, stack, "spawned"))) {
@@ -76,13 +86,15 @@ public class Give extends BukkitCommand {
 			}
 		}
 
-		if (args.length == 0)
-			amount = stack.getType().isBlock() ? NiftyItems.getPluginConfig().getBlockStackSize() : NiftyItems.getPluginConfig().getItemStackSize();
-		else if (NumberUtil.isInt(args[0])) {
-			int passedAmount = Integer.parseInt(args[0]);
-			amount = passedAmount < 0 ? amount : passedAmount;
-			args = StringUtil.split(",", StringUtil.implode(",", args, 1, args.length));
+		if (args.length > 0) {
+			if (NumberUtil.isInt(args[0])) {
+				amount = Integer.parseInt(args[0]);
+				args = StringUtil.split(",", StringUtil.implode(",", args, 1));
+			}
 		}
+
+		if (amount < 0)
+			amount = stack.getType().isBlock() ? NiftyItems.getPluginConfig().getBlockStackSize() : NiftyItems.getPluginConfig().getItemStackSize();
 
 		stack.setAmount(amount);
 		if (!this.hasPermissions(sender, "bypass", "lore")) stack = Lore.apply(sender, stack, Lore.getLore("spawned"));
