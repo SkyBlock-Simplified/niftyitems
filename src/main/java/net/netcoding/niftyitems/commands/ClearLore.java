69441e8bd7c178a35f38275c2326d99217add0de
Brian Graham <support@netcoding.net>
2016-01-26 13:04:55 -0500

    Fixed..

--- src/main/java/net/netcoding/niftyitems/commands/ClearLore.java
+++ src/main/java/net/netcoding/niftyitems/commands/ClearLore.java
@@ -1,8 +1,9 @@
 package net.netcoding.niftyitems.commands;
 
 import net.netcoding.niftybukkit.minecraft.BukkitCommand;
+import net.netcoding.niftycore.util.ListUtil;
+import net.netcoding.niftycore.util.StringUtil;
 import net.netcoding.niftyitems.managers.Lore;
-
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -14,6 +15,7 @@ public class ClearLore extends BukkitCommand {
 		super(plugin, "clearlore");
 		this.setPlayerOnly();
 		this.setMinimumArgsLength(0);
+		this.setMaximumArgsLength(2);
 	}
 
 	@Override
@@ -23,23 +25,33 @@ public class ClearLore extends BukkitCommand {
 			return;
 		}
 
-		Player player = args.length == 1 ? (Player)sender : findPlayer(args[0]);
+		String action = "hand";
+
+		if (ListUtil.notEmpty(args)) {
+			if (args[args.length - 1].matches("(?i)^(hand|all)$")) {
+				action = args[args.length - 1];
+				args = StringUtil.split(" ", StringUtil.implode(" ", args, 1));
+			}
+		}
+
+		Player player = args.length == 0 ? (Player)sender : findPlayer(args[0]);
+		boolean isSelf = sender.getName().equals(player.getName());
 
 		if (player == null) {
 			this.getLog().error(sender, "Unable to locate player matching {{0}}!", args[0]);
 			return;
 		}
 
-		if (args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("hand"))) {
+		if ("hand".equalsIgnoreCase(action)) {
 			if (Lore.revert(player.getItemInHand()))
 				this.getLog().message(sender, "The item {{0}} has had its lore removed.", player.getItemInHand().getType().name());
 			else
 				this.getLog().error(sender, "The item {{0}} has no lore.", player.getItemInHand().getType().name());
-		} else if (args.length == 1 && args[0].equalsIgnoreCase("all")) {
+		} else if ("all".equalsIgnoreCase(action)) {
 			for (ItemStack item : player.getInventory().getContents())
 				Lore.revert(item);
 
-			this.getLog().message(sender, "All items in your inventory have had their removed.");
+			this.getLog().message(sender, "All items {0} have had their removed.", StringUtil.format("{0} {{1}}", (isSelf ? "in" : "for"), (isSelf ? "your inventory" : player.getName())));
 		} else
 			this.showUsage(sender);
 	}
