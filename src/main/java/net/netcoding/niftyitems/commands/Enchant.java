d486b642d2a1f8074039c10faa650c4b2888e33c
Brian Graham <support@netcoding.net>
2016-01-26 13:31:30 -0500

    Better uenchant with no args Added ability to list enchants

--- src/main/java/net/netcoding/niftyitems/commands/Enchant.java
+++ src/main/java/net/netcoding/niftyitems/commands/Enchant.java
@@ -29,59 +29,69 @@ public class Enchant extends BukkitCommand {
 		Player player = (Player)sender;
 		ItemStack stack = player.getItemInHand();
 		List<String> enchants = new ArrayList<>();
+		boolean unsafe = alias.matches("^ue(nchant)?$");
 
-		if (stack == null || Material.AIR.equals(stack.getType())) {
+		if (stack == null || Material.AIR == stack.getType()) {
 			this.getLog().error(sender, "You must be holding an item in order to list or apply enchantments!");
 			return;
 		}
 
-		if (alias.matches("^ue(nchant)?$") && !this.hasPermissions(sender, "enchant", "unsafe")) {
+		if (unsafe && !this.hasPermissions(sender, "enchant", "unsafe")) {
 			this.getLog().error(sender, "You are not allowed to set unsafe enchantments!");
 			return;
 		}
 
 		if (ListUtil.isEmpty(args)) {
-			List<EnchantmentData> possibleEnchants = NiftyBukkit.getEnchantmentDatabase().getPossibleEnchants(stack);
-
-			if (ListUtil.isEmpty(possibleEnchants))
-				this.getLog().error(sender, "The {0} {{1}} in your hand cannot normally be enchanted!", (stack.getType().isBlock() ? "block" : "item"), stack.getType().name());
+			if (unsafe)
+				this.getLog().message(sender, "You can apply any enchant using the {{0}} command! See {{1}}", "/uenchant", "/enchant list");
 			else {
-				List<String> names = new ArrayList<>();
+				List<EnchantmentData> possibleEnchants = NiftyBukkit.getEnchantmentDatabase().getPossibleEnchants(stack);
 
-				for (EnchantmentData enchData : possibleEnchants) {
-					List<String> enchNames = enchData.getNames();
-
-					for (int i = 0; i < (enchNames.size() < 2 ? enchNames.size() : 2); i++)
-						names.add(enchNames.get(i));
-				}
-
-				if (ListUtil.isEmpty(names))
-					this.getLog().error(sender, "Unable to locate any possible enchantments for {{0}}!", stack.getType().name());
+				if (ListUtil.isEmpty(possibleEnchants))
+					this.getLog().error(sender, "The {0} {{1}} in your hand cannot normally be enchanted!", (stack.getType().isBlock() ? "block" : "item"), stack.getType().name());
 				else {
-					this.getLog().message(sender, "[{{0}}]", "Possible Enchantments");
-					this.getLog().message(sender, "{{0}}", StringUtil.implode(ChatColor.GRAY + ", " + ChatColor.RED, names));
+					List<String> names = new ArrayList<>();
+
+					for (EnchantmentData enchData : possibleEnchants) {
+						List<String> enchNames = enchData.getNames();
+
+						for (int i = 0; i < (enchNames.size() < 2 ? enchNames.size() : 2); i++)
+							names.add(enchNames.get(i));
+					}
+
+					if (ListUtil.isEmpty(names))
+						this.getLog().error(sender, "Unable to locate any possible enchantments for {{0}}!", stack.getType().name());
+					else {
+						this.getLog().message(sender, "[{{0}}]", "Possible Enchantments");
+						this.getLog().message(sender, "{{0}}", StringUtil.implode(ChatColor.GRAY + ", " + ChatColor.RED, names));
+					}
 				}
 			}
 		} else {
 			if (args[0].matches("^fake|hidden|glow$")) {
-				if (alias.matches("^ue(nchant)?$") && !this.hasPermissions(sender, "enchant", "unsafe")) {
+				if (unsafe) {
 					stack = ItemData.addGlow(stack);
 					player.setItemInHand(stack);
 					this.getLog().message(sender, "Your item now glows!");
 				} else
 					this.getLog().error(sender, "You are not allowed to a set fake enchantment!");
+			} else if ("list".equalsIgnoreCase(args[0])) {
+				List<String> names = NiftyBukkit.getEnchantmentDatabase().primaryNames();
+				this.getLog().message(sender, "[{{0}}]", "All Enchantments");
+				this.getLog().message(sender, "{{0}}", StringUtil.implode(ChatColor.GRAY + ", " + ChatColor.RED, names));
 			} else {
 				for (EnchantmentData data : NiftyBukkit.getEnchantmentDatabase().parse(args)) {
 					try {
-						if (data.apply(stack, alias.matches("^ue(nchant)?$")))
+						if (data.apply(stack, unsafe))
 							enchants.add(StringUtil.format("{{0}} at level {{1}}.", data.getName(), data.getUserLevel()));
-					} catch (Exception ignored) { }
+					} catch (Exception ignore) { }
 				}
 
 				if (enchants.size() == 0)
 					this.getLog().error(sender, "Unable to apply any enchants, please check your previous command!");
 				else {
 					this.getLog().message(sender, "Your {{0}} has been given the following enchantments:", NiftyBukkit.getItemDatabase().name(stack));
+
 					for (String enchant : enchants)
 						this.getLog().message(sender, enchant);
 				}
