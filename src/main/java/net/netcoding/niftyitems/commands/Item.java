091d8b7d28373f98ef4af5ac3f33336ea27ca991
Brian Graham <support@netcoding.net>
2016-04-21 18:26:07 -0400

    Switched to use ItemData

--- src/main/java/net/netcoding/niftyitems/commands/Item.java
+++ src/main/java/net/netcoding/niftyitems/commands/Item.java
@@ -1,8 +1,9 @@
 package net.netcoding.niftyitems.commands;
 
 import net.netcoding.niftybukkit.NiftyBukkit;
-import net.netcoding.niftybukkit.minecraft.inventory.InventoryWorkaround;
 import net.netcoding.niftybukkit.minecraft.BukkitCommand;
+import net.netcoding.niftybukkit.minecraft.inventory.InventoryWorkaround;
+import net.netcoding.niftybukkit.minecraft.items.ItemData;
 import net.netcoding.niftybukkit.mojang.BukkitMojangProfile;
 import net.netcoding.niftycore.mojang.exceptions.ProfileNotFoundException;
 import net.netcoding.niftycore.util.NumberUtil;
@@ -13,7 +14,6 @@ import net.netcoding.niftyitems.managers.Lore;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Item extends BukkitCommand {
@@ -21,7 +21,6 @@ public class Item extends BukkitCommand {
 	public Item(JavaPlugin plugin) {
 		super(plugin, "item");
 		this.setCheckPerms(false);
-		this.setCheckHelp(false);
 		this.editUsage(0, "give", "<player> <type>[:datavalue] [amount]");
 		this.editUsage(0, "g", "<player> <type>[:datavalue] [amount]");
 	}
@@ -59,13 +58,13 @@ public class Item extends BukkitCommand {
 			}
 		}
 
-		ItemStack stack;
+		ItemData itemData;
 		Player player = receiver.getOfflinePlayer().getPlayer();
 
 		try {
-			stack = NiftyBukkit.getItemDatabase().get(args[0]);
+			itemData = NiftyBukkit.getItemDatabase().get(args[0]);
 
-			if (Material.AIR == stack.getType()) {
+			if (Material.AIR == itemData.getType()) {
 				this.getLog().error(sender, "You cannot spawn {0}!", "AIR");
 				return;
 			}
@@ -74,13 +73,13 @@ public class Item extends BukkitCommand {
 			return;
 		}
 
-		String displayName = NiftyBukkit.getItemDatabase().name(stack);
-		displayName = StringUtil.isEmpty(displayName) ? stack.getType().name() : displayName;
+		String displayName = NiftyBukkit.getItemDatabase().name(itemData);
+		displayName = StringUtil.isEmpty(displayName) ? itemData.getType().name() : displayName;
 		args = StringUtil.split(",", StringUtil.implode(",", args, 1));
 		int amount = -1;
 
 		if (isItem || (NiftyItems.getPluginConfig().giveEnforcesBlacklist() && !this.hasPermissions(sender, "bypass", "give"))) {
-			if (NiftyItems.getPluginConfig().isBlacklisted(player, stack, "spawned") || (!this.hasPermissions(player, "item") && !NiftyItems.getPluginConfig().hasBypass(player, stack, "spawned"))) {
+			if (NiftyItems.getPluginConfig().isBlacklisted(player, itemData, "spawned") || (!this.hasPermissions(player, "item") && !NiftyItems.getPluginConfig().hasBypass(player, itemData, "spawned"))) {
 				if (!NiftyItems.getPluginConfig().isSilent("spawned"))
 					this.getLog().error(sender, "You cannot spawn {{0}}{1}!", displayName, (isGive ? StringUtil.format(" for {{0}}", player.getName()) : ""));
 
@@ -96,20 +95,22 @@ public class Item extends BukkitCommand {
 		}
 
 		if (amount < 0)
-			amount = stack.getType().isBlock() ? NiftyItems.getPluginConfig().getBlockStackSize() : NiftyItems.getPluginConfig().getItemStackSize();
+			amount = itemData.getType().isBlock() ? NiftyItems.getPluginConfig().getBlockStackSize() : NiftyItems.getPluginConfig().getItemStackSize();
 
-		stack.setAmount(amount);
-		if (!this.hasPermissions(sender, "bypass", "lore")) stack = Lore.apply(sender, stack, Lore.getLore("spawned"));
+		itemData.setAmount(amount);
+
+		if (!this.hasPermissions(sender, "bypass", "lore"))
+			itemData = Lore.apply(sender, itemData, Lore.getLore("spawned"));
 
 		if (args.length > 0 && this.hasPermissions(sender, "item", "data")) {
 			// TODO: Item Data
 		}
 
 		if (this.hasPermissions(sender, "bypass", "stacksize"))
-			InventoryWorkaround.addOversizedItems(player.getInventory(), Config.MAXIMUM_OVERSTACKED_SIZE, stack);
+			InventoryWorkaround.addOversizedItems(player.getInventory(), Config.MAXIMUM_OVERSTACKED_SIZE, itemData);
 		else
-			InventoryWorkaround.addItems(player.getInventory(), stack);
+			InventoryWorkaround.addItems(player.getInventory(), itemData);
 
-		this.getLog().message(sender, "Giving {{0}} of {{1}}.", stack.getAmount(), displayName);
+		this.getLog().message(sender, "Giving {{0}} of {{1}}.", itemData.getAmount(), displayName);
 	}
 }
